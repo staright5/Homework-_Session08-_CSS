568d77f1fd38e6848d088b929834059c1165e43f
staright5 <nu230199@gmail.com>
2024-09-16 20:50:18 +0900

    Add files via upload

---
+++ java.js
@@ -0,0 +1,82 @@
+//Homework1
+
+//alert("Xin Chào");
+
+//Homework2
+
+// let i = 10;
+// let f = 20.5;
+// let b = true;
+// let s = "Hà Nội";
+
+// console.log(i);
+// console.log(f);
+// console.log(b);
+// console.log(s);
+
+//Homework3
+
+// let height = 10;
+// let width = 5;
+// let area = height * width;
+// console.log("Diện tích hình chữ nhật là: " + area);
+
+//Homework4
+
+//confirm("Bạn đã đủ 18 tuổi chưa!");
+
+//Homework5
+
+// let a = prompt("Điểm Vật Lý:");
+// let b = prompt("Điểm Hóa Học:");
+// let c = prompt("Điểm Sinh Học:");
+
+// let average = (+a + +b + +c) / 3;
+// console.log("Điểm trung bình của bạn là: " + average);
+
+//Homework6
+
+// let C = prompt("Nhập nhiệt độ theo độ Celsius:");
+// let F = (C / 5) * 9 + 32;
+// console.log("Nhiệt độ theo độ Fahrenheit là: " + F);
+
+//Homework7
+
+// let height = 10;
+// let width = 5;
+// let area = height * width;
+// let perimeter = 2 * (height + width);
+// console.log("Diện tích hình chữ nhật là: " + area);
+// console.log("Chu vi hình chữ nhật là: " + perimeter);
+
+//Homework8
+
+// const pi = 3.14;
+// let radius = prompt("Nhập bán kính hình tròn:");
+// let perimeter = pi * radius * 2;
+// let area = pi * radius * radius;
+// console.log("Chu vi hình tròn là: " + perimeter);
+// console.log("Diện tích hình tròn là: " + area);
+
+//Homework9
+
+// let a = 5;
+// let b = "5";
+// console.log(a == b);
+// console.log(a === b);
+// console.log(a != b);
+// console.log(a !== b);
+
+// == đưa ra giá trị true bởi vì nó đã hoán đổi biến b từ string thành số rồi so sánh
+// === đưa ra giá trị false bởi vì nó không đổi => khác nhau về kiểu dữ liệu
+// != và !== hoạt động giống == với === nhưng ngược lại
+
+//Homework10
+
+// let a = 6;
+// let result = ++a + a + a++ + ++a + a;
+// console.log(result); // 39
+
+// ++a = pre-increment => tăng giá trị biến a lên 1 và trả về giá trị mới
+// a++ = post-increment => sự dụng giá trị của biến a trước khi tăng giá trị lên 1
+// Kết quả = 7 + 7 + 7 + 9 + 9 = 39
